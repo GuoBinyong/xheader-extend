286a525f93c80a83bc2d2ebfb3e3c370988a811a
郭斌勇 <guobinyong@qq.com>
2020-05-19 14:45:46 +0800

    完成类型定义

--- src/index.js
+++ src/index.js
@@ -1,196 +1,232 @@
-import {XHeader} from 'vux';
-import {parseButtonOptionsListFromVNodeList} from '@tools/VNode'
+import { XHeader } from 'vux';
+import { parseButtonOptionsListFromVNodeList } from 'vnode-tls'
 
 
-let agentMixin = {
-  computed: {
+/**
+ * 根据增强选项创建新的 Header 的组件选项
+ * @param options : EnhanceOptions      必须；增强选项
+ * @returns ComponentOptions   返回新创建的增强后的 Header 组件选项
+ */
+export function createHeader(options) {
+  let NewHeader = Object.assign({},XHeader);
+  return enhanceHeader(NewHeader,options);
+}
 
-
-    /**
-     *
-     * @return {{back: {hide: boolean, text: *}, preventBack: *, buttonOptionsList: *[]}}
-     */
-    navLeftOptions: function () {
-
-      //props
-      let {showBack, backText, preventGoBack} = this.leftOptions || {};
-      let onClickBack = this.$listeners["on-click-back"];
-
-      let backShow = showBack === false ? false : true;
-      let back = {
-        hide: !backShow,
-        text: backText,
-        click: onClickBack
-      };
-
-      // slot: overwrite-left
-      let overwriteLeftVNodes = this.$slots.overwriteLeft;
-      let owLeftSlotBtnOptionsList = null;
-      if (overwriteLeftVNodes) {
-        back.hide = true;
-        owLeftSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(overwriteLeftVNodes);
-      }
-
-
-      //slot: overwrite-left
-      let leftVNodes = this.$slots.left;
-      let leftSlotBtnOptionsList = null;
-      if (leftVNodes) {
-        leftSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(leftVNodes);
-      }
-
-
-      let leftBtnOptionsList = null;
-
-      if (owLeftSlotBtnOptionsList || leftSlotBtnOptionsList) {
-        leftBtnOptionsList = [...owLeftSlotBtnOptionsList, ...leftSlotBtnOptionsList];
-      }
-
-
-      let leftOptions = {
-        back: back,
-        preventBack: preventGoBack,
-        buttonOptionsList: leftBtnOptionsList
-      };
-
-
-      return leftOptions;
-
-    },
-
-
-    /**
-     *
-     * @return {{title: {hide: boolean, text: *, click: *}, buttonOptionsList: *}}
-     */
-    navTitleOptions: function () {
-      let title = this.title;
-      let onClickTitle = this.$listeners["on-click-title"];
-
-      let titleBtnOptions = {
-        hide: false,
-        text: title,
-        click: onClickTitle
-      };
-
-      let slotList = this.$slots;
-      let titleVNodes = slotList.overwriteTitle || slotList.default;
-      let titleSlotBtnOptionsList = null;
-      if (titleVNodes) {
-        titleBtnOptions.hide = true;
-        titleSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(titleVNodes);
-      }
-
-
-      let titleOptions = {
-        title: titleBtnOptions,
-        buttonOptionsList: titleSlotBtnOptionsList
-      };
-
-
-      return titleOptions;
-
-
-    },
-
-
-    /**
-     *
-     * @return {{more: {hide: boolean, click: *}, buttonOptionsList: *}}
-     */
-    navRightOptions: function () {
-      let showMore = this.rightOptions && this.rightOptions.showMore;
-
-      let onClickMore = this.$listeners["on-click-more"];
-
-      let moreBtnOptions = {
-        hide: !showMore,
-        click: onClickMore
-      };
-
-      let rightVNodes = this.$slots.right;
-      let rightSlotBtnOptionsList = null;
-      if (rightVNodes) {
-        rightSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(rightVNodes);
-      }
-
-
-      let rightOptions = {
-        more: moreBtnOptions,
-        buttonOptionsList: rightSlotBtnOptionsList
-      };
-
-
-      return rightOptions;
-    },
-
-
-    navBarOptions: function () {
-      let leftOptions = this.navLeftOptions;
-      let titleOptions = this.navTitleOptions;
-      let rightOptions = this.navRightOptions;
-
-      let navBarOptions = {
-        left: leftOptions,
-        title: titleOptions,
-        right: rightOptions
-      };
-
-      return navBarOptions;
-    }
-
-
-  },
-
-
-  watch: {
-    navBarOptions: function (newVal, oldVal) {
-      this.updateNavBarOptions(newVal);
-    }
-  },
-
-  mounted: function () {
-    this.updateNavBarOptions(this.navBarOptions);
-  },
-
-  activated:function(){
-    this.updateNavBarOptions(this.navBarOptions);
-  },
-
-  methods: {
-    updateNavBarOptions: function (newNavBarOptions) {
-      shareInst.client.updateNavBarOptions(newNavBarOptions);
-    }
-
-  },
-
-
-  created: function () {
-    this.clientChangeHandle = () => {
-      this.$forceUpdate();
-      this.updateNavBarOptions(this.navBarOptions);
-    };
-    window.byAddEventListener("clientChange", this.clientChangeHandle);
-  },
-  beforeDestroy: function () {
-    window.byRemoveEventListener("clientChange", this.clientChangeHandle);
-  }
-
-
-};
-
-
-let xHeaderMixins = XHeader.mixins || [];
-
-xHeaderMixins = xHeaderMixins.concat(agentMixin);
-
-XHeader.mixins = xHeaderMixins;
-
-
-let oriRender = XHeader.render;
-XHeader.render = function (h) {
-  return shareInst.client.showWebNavBar ? oriRender.call(this, h) : null;
+/**
+ * 根据增强选项扩展原来的 XHeader 组件选项
+ * @param options : EnhanceOptions      必须；增强选项
+ * @returns ComponentOptions   返回已扩展的 XHeader 组件选项
+ */
+export function expandXHeader(options) {
+  return enhanceHeader(XHeader,options);
 }
 
 
-export default XHeader;
+
+function enhanceHeader(Header, { updateNavBarOptions, hide = false }) {
+
+  if (Header.__NavBarOptions__){
+    return Header;
+  }else {
+    Header.__NavBarOptions__ = true;
+  }
+
+  var hideCB = typeof hide == "function" ? hide : function () { return hide };
+
+
+
+  let agentMixin = {
+    computed: {
+
+
+      /**
+       *
+       * @return {{back: {hide: boolean, text: *}, preventBack: *, buttonOptionsList: *[]}}
+       */
+      navLeftOptions: function () {
+
+        //props
+        let { showBack, backText, preventGoBack } = this.leftOptions || {};
+        let onClickBack = this.$listeners["on-click-back"];
+
+        let backShow = showBack === false ? false : true;
+        let back = {
+          hide: !backShow,
+          text: backText,
+          click: onClickBack
+        };
+
+        // slot: overwrite-left
+        let overwriteLeftVNodes = this.$slots.overwriteLeft;
+        let owLeftSlotBtnOptionsList = null;
+        if (overwriteLeftVNodes) {
+          back.hide = true;
+          owLeftSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(overwriteLeftVNodes);
+        }
+
+
+        //slot: overwrite-left
+        let leftVNodes = this.$slots.left;
+        let leftSlotBtnOptionsList = null;
+        if (leftVNodes) {
+          leftSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(leftVNodes);
+        }
+
+
+        let leftBtnOptionsList = null;
+
+        if (owLeftSlotBtnOptionsList || leftSlotBtnOptionsList) {
+          leftBtnOptionsList = [...owLeftSlotBtnOptionsList, ...leftSlotBtnOptionsList];
+        }
+
+
+        let leftOptions = {
+          back: back,
+          preventBack: preventGoBack,
+          buttonOptionsList: leftBtnOptionsList
+        };
+
+
+        return leftOptions;
+
+      },
+
+
+      /**
+       *
+       * @return {{title: {hide: boolean, text: *, click: *}, buttonOptionsList: *}}
+       */
+      navTitleOptions: function () {
+        let title = this.title;
+        let onClickTitle = this.$listeners["on-click-title"];
+
+        let titleBtnOptions = {
+          hide: false,
+          text: title,
+          click: onClickTitle
+        };
+
+        let slotList = this.$slots;
+        let titleVNodes = slotList.overwriteTitle || slotList.default;
+        let titleSlotBtnOptionsList = null;
+        if (titleVNodes) {
+          titleBtnOptions.hide = true;
+          titleSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(titleVNodes);
+        }
+
+
+        let titleOptions = {
+          title: titleBtnOptions,
+          buttonOptionsList: titleSlotBtnOptionsList
+        };
+
+
+        return titleOptions;
+
+
+      },
+
+
+      /**
+       *
+       * @return {{more: {hide: boolean, click: *}, buttonOptionsList: *}}
+       */
+      navRightOptions: function () {
+        let showMore = this.rightOptions && this.rightOptions.showMore;
+
+        let onClickMore = this.$listeners["on-click-more"];
+
+        let moreBtnOptions = {
+          hide: !showMore,
+          click: onClickMore
+        };
+
+        let rightVNodes = this.$slots.right;
+        let rightSlotBtnOptionsList = null;
+        if (rightVNodes) {
+          rightSlotBtnOptionsList = parseButtonOptionsListFromVNodeList(rightVNodes);
+        }
+
+
+        let rightOptions = {
+          more: moreBtnOptions,
+          buttonOptionsList: rightSlotBtnOptionsList
+        };
+
+
+        return rightOptions;
+      },
+
+
+      navBarOptions: function () {
+        let leftOptions = this.navLeftOptions;
+        let titleOptions = this.navTitleOptions;
+        let rightOptions = this.navRightOptions;
+
+        let navBarOptions = {
+          left: leftOptions,
+          title: titleOptions,
+          right: rightOptions
+        };
+
+        return navBarOptions;
+      }
+
+
+    },
+
+
+    watch: {
+      navBarOptions: function (newVal, oldVal) {
+        this.updateNavBarOptions(newVal);
+      }
+    },
+
+    mounted: function () {
+      this.updateNavBarOptions(this.navBarOptions);
+    },
+
+    activated: function () {
+      this.updateNavBarOptions(this.navBarOptions);
+    },
+
+    methods: {
+      updateNavBarOptions: function (newNavBarOptions) {
+        updateNavBarOptions.call(this, newNavBarOptions, this);
+      }
+
+    },
+
+
+    created: function () {
+      this.updateNavBarHandle = () => {
+        this.$forceUpdate();
+        this.updateNavBarOptions(this.navBarOptions);
+      };
+      window.addEventListener("updateNavBar", this.updateNavBarHandle);
+    },
+    beforeDestroy: function () {
+      window.removeEventListener("updateNavBar", this.updateNavBarHandle);
+    }
+
+
+  };
+
+
+  let headerMixins = Header.mixins || [];
+
+  headerMixins = headerMixins.concat(agentMixin);
+
+  Header.mixins = headerMixins;
+
+
+  let oriRender = Header.render;
+  Header.render = function (h) {
+    return hideCB(this) ? null : oriRender.call(this, h);
+  }
+
+  return Header;
+
+}
+
+
---
+++ types/index.d.ts
@@ -0,0 +1,105 @@
+import Vue,{ComponentOptions} from "vue"
+
+
+/**
+ * 按钮选项
+ */
+export interface ButtonOptions {
+    text: string;       //按钮的文本
+    hide?:boolean;      //是否隐藏
+    click?: Function;    //按钮的点击事件回调函数；
+}
+
+
+/**
+ * 导航条的组成部件的基本选项
+ * 导航条一般由左、中、右 三部分组成；
+ */
+export interface NavPart {
+    buttonOptionsList?: ButtonOptions[]|null;      //按钮的选项数组
+}
+
+
+/**
+ * 导航条的左部分选项
+ */
+export interface NavLeftOptions extends NavPart {
+    back: ButtonOptions;
+    preventBack ?: boolean;    //是否阻止返回
+}
+
+
+/**
+ * 导航条的标题部分(也就是中间部分)选项
+ */
+export interface NavTitleOptions extends NavPart {
+    title: ButtonOptions;
+}
+
+
+/**
+ * 导航条的右部分选项
+ */
+export interface NavRightOptions extends NavPart {
+    more: ButtonOptions;
+}
+
+
+/**
+ * 导航条的选项
+ */
+export interface NavBarOptions {
+    left: NavLeftOptions;   //导航条的左部分选项
+    title: NavTitleOptions;  //导航条的标题部分(也就是中间部分)选项
+    right: NavRightOptions;  //导航条的右部分选项
+}
+
+
+
+/**
+ * 更改导航条选项的回调函数；
+ */
+export type UpdateNavBarOptions = (this:Vue,navBarOptions:NavBarOptions,headerInst:Vue)=>void;
+
+export type HideGet = (headerInst:Vue)=>boolean; 
+export type Hide = boolean | HideGet;
+
+
+/**
+ * Header 的增强配置选项
+ */
+export interface EnhanceOptions {
+    updateNavBarOptions:UpdateNavBarOptions;   //更改导航条选项的回调函数；会在 Header 有变化时自动调用，使用者可在该回调函数中获取导航条的配置选项
+    hide?: Hide;  //可选；是否隐藏 Header
+}
+
+
+/**
+ * 根据增强选项创建新的 Header 的组件选项
+ * @param options : EnhanceOptions      必须；增强选项
+ * @returns ComponentOptions   返回新创建的增强后的 Header 组件选项
+ */
+export function createHeader(options:EnhanceOptions):ComponentOptions;
+
+
+/**
+ * 根据增强选项扩展原来的 XHeader 组件选项
+ * @param options : EnhanceOptions      必须；增强选项
+ * @returns ComponentOptions   返回已扩展的 XHeader 组件选项
+ */
+export function expandXHeader(options:EnhanceOptions):ComponentOptions;
+
+
+
+/**
+ * 根据增加配置选项来增强指定的 Header
+ * @param Header : ComponentOptions    必须；被增强的 Header 的组件选项；
+ * @param options : EnhanceOptions    必须；增强选项
+ */
+function enhanceHeader(Header:ComponentOptions, options:EnhanceOptions):ComponentOptions;
+
+
+
+
+
+
