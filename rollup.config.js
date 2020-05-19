275311e8205341d4f0483d468358e4ed3b828f41
郭斌勇 <guobinyong@qq.com>
2020-05-19 15:09:28 +0800

    完善配置

--- rollup.config.js
+++ rollup.config.js
@@ -54,6 +54,7 @@ export default [
 	*/
 	{
 		input: 'src/index',
+		external: getDependencieNames(pkg,"peerDependencies"),  //移除 package.json 中 peerDependencies 下的所有的依赖包
 		output: {
 			name: toHumpFormat(pkg.name),  //驼峰格式的 pkg.name
 			// 如果 pkg.browser 是字符串类型，则 file 为 pkg.browser，否则为 `<包名>.umd.js`
